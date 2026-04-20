642524ce150d319e4159b5c34969b94a8af32631
Danielson <lazybumdan@gmail.com>
2026-04-20 10:52:21 +0400

    Added the ability for the code to break
    Just added a while loop to ensure that the program runs for as long as needed.

--- Calculator.py
+++ Calculator.py
@@ -1,25 +1,33 @@
 # A very simple python calculator
 print("Welcome to Daniel's calculator! \n'It can add, subtract, multiply and divide any number!")
 def calculator():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    operator = input("Enter a operator, the options are +,-,*,/,%,**: ")
-    if operator == "+":
-        print(num1 + num2)
-    elif operator == "-":
-        print(num1 - num2)
-    elif operator == "*":
-        print(num1 * num2)
-    elif operator == "/":
-        print(num1 / num2)
-    elif operator == "%":
-        print(num1 % num2)
-    elif operator == "**":
-        print(num1 ** num2)
-    else:
-        print("Invalid operator")
-
+    while True:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        operator = input("Enter a operator, the options are +,-,*,/,%,**: ")
+        if operator == "+":
+            ans = (num1 + num2)
+            print(ans)
+        elif operator == "-":
+            ans = (num1 - num2)
+            print(ans)
+        elif operator == "*":
+            ans = (num1 * num2)
+            print(ans)
+        elif operator == "/":
+            ans = (num1 / num2)
+            print(ans)
+        elif operator == "%":
+            ans = (num1 % num2)
+            print(ans)
+        elif operator == "**":
+            ans = (num1 ** num2)
+            print(ans)
+        simplify = input("Do you want to round the calculation? (y/n): ")
+        if simplify == "y":
+            print(int(ans))
+        else:
+            break
 
 
 calculator()
-# Version 1.0 for the calculator
